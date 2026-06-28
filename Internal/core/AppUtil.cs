6bc4415410988b108bdd16cc7a157a129d01a1df
taiyakihexencoder <nameswithb@gmail.com>
2026-06-29 00:47:15 +0900

    アプリケーションの終了処理

---
+++ Internal/core/AppUtil.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace hexegeer.internallib {
+	public static class AppUtil {
+		public delegate void QuittingHandler();
+
+		public static QuittingHandler quitting = null;
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+		private static void Init() {
+			Application.quitting += Quitting;
+		}
+
+		private static void Quitting() {
+			if (quitting != null) {
+				quitting.Invoke();
+				quitting = null;
+			}
+		}
+
+		public static void Quit() {
+#if UNITY_EDITOR
+			Quitting();
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+		}
+	}
+}
---
+++ Runtime/core/HexegeerUtility.App.cs
@@ -0,0 +1,14 @@
+﻿namespace hexegeer {
+	using internallib;
+
+	public static partial class HexegeerUtility {
+		public static class App {
+			public static event AppUtil.QuittingHandler quitting {
+				add { AppUtil.quitting += value; }
+				remove { AppUtil.quitting -= value; }
+			}
+
+			public static void Quit() { AppUtil.Quit(); }
+		}
+	}
+}
