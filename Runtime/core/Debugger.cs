ac7aa5c6a8a97b61cd3c7eed902d7b9e61b552c4
taiyakihexencoder <nameswithb@gmail.com>
2026-06-30 20:38:07 +0900

    関数名修正

--- Runtime/core/Debugger.cs
+++ Runtime/core/Debugger.cs
@@ -1,7 +1,7 @@
-namespace hexegeer {
+﻿namespace hexegeer {
 	public static class D {
-		public static void Debug(object obj) { internallib.D.Log(obj); }
-		public static void DebugW(object obj) { internallib.D.LogW(obj); }
-		public static void DebugE(object obj) { internallib.D.LogE(obj); }
+		public static void Log(object obj) { internallib.D.Log(obj); }
+		public static void LogW(object obj) { internallib.D.LogW(obj); }
+		public static void LogE(object obj) { internallib.D.LogE(obj); }
 	}
 }
