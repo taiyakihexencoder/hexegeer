ce5ac9206aa2260c0f1cf40717a706fcb0828b61
taiyakihexencoder <nameswithb@gmail.com>
2026-08-11 22:28:14 +0900

    PhysicsColliderの自動破棄機能を作成

---
+++ Internal/geometrycleanup/GeometryCleanup.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+
+namespace hexegeer.internallib {
+	public struct GeometryCleanup : IComponentData, ICleanupComponentData {
+		public BlobAssetReference<Collider> geometry; 
+	}
+}
---
+++ Internal/geometrycleanup/GeometryCleanupSystem.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Physics;
+
+namespace hexegeer.internallib {
+	/// <summary>
+	/// PhysicsColliderを持つEntityが破棄されたらgeometryを解放する
+	/// </summary>
+	[UpdateInGroup(typeof(HexegeerSimulationSystemGroup))]
+	public partial struct GeometryCleanupSystem : ISystem {
+		private EntityQuery _query;
+
+		void ISystem.OnCreate(ref SystemState state) {
+			_query = new EntityQueryBuilder(Allocator.Temp)
+				.WithOptions(EntityQueryOptions.IncludePrefab)
+				.WithAll<GeometryCleanup>()
+				.WithNone<PhysicsCollider>()
+				.Build(ref state);
+			state.RequireForUpdate(_query);
+		}
+
+		void ISystem.OnUpdate(ref SystemState state) {
+			state.Dependency = new ReleaseJob { }.ScheduleParallel(_query, state.Dependency);
+		}
+	
+		/// <summary>
+		/// Editorの再生終了など、終了処理の場合はEntityが破棄されずLeakになってしまうので、
+		/// OnDestroyですべて破棄する。
+		/// </summary>
+		void ISystem.OnDestroy(ref SystemState state) {
+			EntityQuery destroyQuery = new EntityQueryBuilder(Allocator.Temp)
+				.WithOptions(EntityQueryOptions.IncludePrefab)
+				.WithAll<GeometryCleanup>()
+				.Build(ref state);
+
+			NativeArray<GeometryCleanup> array = destroyQuery.ToComponentDataArray<GeometryCleanup>(Allocator.Temp);
+			foreach(GeometryCleanup cleanup in array) {
+				cleanup.geometry.Dispose();
+			}
+			array.Dispose();
+		}
+
+		partial struct ReleaseJob : IJobEntity {
+			void Execute(RefRO<GeometryCleanup> cleanup) {
+				cleanup.ValueRO.geometry.Dispose();
+			}
+		}
+	}
+
+}
