cf711db4d276d339ae668cd68d2f824405141a93
taiyakihexencoder <nameswithb@gmail.com>
2026-08-16 18:59:01 +0900

    Limited Life Time実装

---
+++ Internal/patterns/limitedlifetime/LimitedLifeTime.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+namespace hexegeer.internallib {
+	public struct LimitedLifeTime : IComponentData {
+		public float seconds;
+	}
+}
---
+++ Internal/patterns/limitedlifetime/LimitedLifeTimeSystem.cs
@@ -0,0 +1,45 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace hexegeer.internallib {
+	[UpdateInGroup(typeof(HexegeerSimulationSystemGroup))]
+	public partial struct LimitedLifeTimeSystem : ISystem {
+		private EntityQuery _query;
+
+		void ISystem.OnCreate(ref SystemState state) {
+			_query = new EntityQueryBuilder(Allocator.Temp)
+				.WithAllRW<LimitedLifeTime>()
+				.Build(ref state);
+			state.RequireForUpdate(_query);
+		}
+
+		void ISystem.OnUpdate(ref SystemState state) {
+			state.Dependency = new ProcessJob {
+				commandBuffer = CreateCommandBuffer(ref state).AsParallelWriter(),
+				deltaTime = SystemAPI.Time.DeltaTime,
+			}.ScheduleParallel(_query, state.Dependency);
+		}
+	
+		void ISystem.OnDestroy(ref SystemState state) {
+		}
+
+		private readonly EntityCommandBuffer CreateCommandBuffer(ref SystemState state) {
+			return SystemAPI
+				.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+				.CreateCommandBuffer(state.World.Unmanaged);
+		}
+
+		partial struct ProcessJob : IJobEntity {
+			public EntityCommandBuffer.ParallelWriter commandBuffer;
+			[ReadOnly] public float deltaTime;
+
+			void Execute([EntityIndexInQuery] int sortKey, in Entity entity, RefRW<LimitedLifeTime> lifeTime) {
+				if (lifeTime.ValueRO.seconds < deltaTime) {
+					commandBuffer.DestroyEntity(sortKey, entity);
+				} else {
+					lifeTime.ValueRW.seconds -= deltaTime;
+				}
+			}
+		}
+	}
+}
