aef8b428e5e29dbe4969a3672b3f5733ba5b1ccf
taiyakihexencoder <nameswithb@gmail.com>
2026-08-21 00:05:58 +0900

    シェーダーのVector2, Vector3表示

---
+++ Editor/shader/Vector2Drawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace hexegeer.editor {
+	public sealed class Vector2Drawer : MaterialPropertyDrawer {
+		public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor) {
+			using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope()) {
+				EditorGUIUtility.labelWidth /= 3;
+				Vector2 value = EditorGUI.Vector2Field(position, label, new Vector2(prop.vectorValue.x, prop.vectorValue.y));
+
+				if (scope.changed) {
+					prop.vectorValue = new Vector4(value.x, value.y, 0f, 0f);
+				}
+			}
+		}
+	}
+}
---
+++ Editor/shader/Vector3Drawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace hexegeer.editor {
+	public sealed class Vector3Drawer : MaterialPropertyDrawer {
+		public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor) {
+			using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope()) {
+				EditorGUIUtility.labelWidth /= 3;
+				Vector3 value = EditorGUI.Vector3Field(position, label, new Vector3(prop.vectorValue.x, prop.vectorValue.y, prop.vectorValue.z));
+
+				if (scope.changed) {
+					prop.vectorValue = new Vector4(value.x, value.y, value.z, 0f);
+				}
+			}
+		}
+	}
+}
