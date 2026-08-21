4174b6f5e34f21528bfac6adb2fba1bb8261f1e2
taiyakihexencoder <nameswithb@gmail.com>
2026-08-22 02:41:25 +0900

    カメラ行列を保持して三次元位置→スクリーン座標変換ができるようにする

---
+++ Runtime/camera/component/CameraMatrix.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace hexegeer {
+	public struct CameraMatrix : IComponentData {
+		public float4x4 viewMatrix;
+		public float4x4 projectMatrix;
+
+		/// <summary>
+		/// 画面内であれば、x,y座標は0-1の座標、z座標は0より大きい。
+		/// それ以外の場合はカメラ範囲に存在しない。
+		/// z座標が負の場合はカメラの裏側。
+		/// </summary>
+		public readonly float3 GetScreenPos(in float3 worldPosition) {
+			float4x4 vpMat = math.mul(projectMatrix, viewMatrix);
+
+			float4 clipSpace = math.mul(vpMat, new float4(worldPosition, 1.0f));
+
+			if (clipSpace.w == 0.0f) {
+				return new float3(0f,0f,0f);
+			} else {
+				return new float3(
+					(clipSpace.x / clipSpace.w + 1f) * 0.5f,
+					(clipSpace.y / clipSpace.w + 1f) * 0.5f,
+					clipSpace.w
+				);
+			}
+		}
+	}
+}
---
+++ Runtime/camera/system/CameraExtractMatrixSystem.cs
@@ -0,0 +1,30 @@
+﻿using hexegeer.internallib;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace hexegeer {
+	[UpdateInGroup(typeof(HexegeerCameraSystemGroup))]
+	public partial class CameraExtractMatrixSystem : SystemBase {
+		protected override void OnCreate() {
+			base.OnCreate();
+			EntityManager.CreateSingleton(
+				new CameraMatrix { 
+					projectMatrix = float4x4.identity, 
+					viewMatrix = float4x4.identity,
+				}, 
+				"Camera Matrix@Hexegeer"
+			);
+		}
+
+		protected override void OnUpdate() {
+			Camera camera = Camera.main;
+			if (camera != null) {
+				SystemAPI.SetSingleton(new CameraMatrix {
+					viewMatrix = camera.worldToCameraMatrix,
+					projectMatrix = GL.GetGPUProjectionMatrix(camera.projectionMatrix, false),
+				});
+			}
+		}
+	}
+}
