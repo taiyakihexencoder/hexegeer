1eed64d8989a8e64063386df36b92d919ce50c00
taiyakihexencoder <nameswithb@gmail.com>
2026-08-21 20:15:41 +0900

    ポストプロセス管理スクリプト

---
+++ Runtime/shader/PostProcessControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace hexegeer {
+	public sealed class PostProcessControl : MonoBehaviour {
+		private static PostProcessControl _instance;
+		private VolumeProfile _profile;
+
+		private void Awake() {
+			_instance = this;
+			_profile = GetComponent<VolumeProfile>();
+		}
+
+		public static bool TryGetProfile<T>(out T volume) where T: VolumeComponent {
+			return _instance._profile.TryGet(out volume);
+		}
+	}
+}
